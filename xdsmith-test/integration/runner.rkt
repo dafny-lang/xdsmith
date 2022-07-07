42c2c1efe5029a08784bc3385356f68cee9b399c
Sorawee Porncharoenwase <soraweep@amazon.com>
2022-07-07 02:19:01 -0700

    +end-to-end test

--- xdsmith-test/integration/runner.rkt
+++ xdsmith-test/integration/runner.rkt
@@ -17,10 +17,32 @@
 
   (current-directory "../../work-dir")
 
+  ;; Testing the oracle (known good file)
+
   (for ([good-path good-paths])
     (with-check-info (['path good-path])
       (check-equal? (oracle good-path) 'ok)))
 
+  ;; Testing the oracle (known bad files)
+
   (for ([bad-path bad-paths])
     (with-check-info (['path bad-path])
-      (check-equal? (oracle bad-path) 'known-mismatch))))
+      (check-equal? (oracle bad-path) 'known-mismatch)))
+
+  ;; End-to-end test with a fixed seed
+
+  (define tmp.dfy (make-temporary-file "tmp~a.dfy"))
+  (with-output-to-file tmp.dfy
+    #:exists 'replace
+    (λ ()
+      (system* (find-executable-path "racket")
+               "../xdsmith/fuzzer.rkt"
+               "--seed" "42"
+               "--dafny-syntax" "true")))
+  (displayln "== Example generated file ==")
+  (display (file->string tmp.dfy))
+
+  (check-true
+   (system* (find-executable-path "racket")
+            "../xdsmith/differ.rkt"
+            tmp.dfy)))
